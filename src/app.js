44d56ce234ef6bae63e1baa7714c13d265c5494f
Dani Cornejo <dcornejo.h@gmail.com>
2025-04-25 16:10:58 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
+  let who = ['El perro', 'Mi abuela', 'El cartero', 'Mi pájaro'];
+  let action = ['comió', 'orinó sobre', 'rompió', 'aplastó'];
+  let what = ['mi tarea', 'mi teléfono', 'el auto'];
+  let when = [
+    'antes de la clase',
+    'mientras dormía',
+    'cuando hacía ejercicio',
+    'durante el almuerzo',
+    'mientras rezaba'
+  ];
 
+  let excuse =
+    who[Math.floor(Math.random() * who.length)] + ' ' +
+    action[Math.floor(Math.random() * action.length)] + ' ' +
+    what[Math.floor(Math.random() * what.length)] + ' ' +
+    when[Math.floor(Math.random() * when.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById('excuse').innerHTML = excuse;
 };
